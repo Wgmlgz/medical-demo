76859dcf9d78c4b89d4c566dd95688a0e0bb5dbb
Wgmlgz <62064707+Wgmlgz@users.noreply.github.com>
2023-06-23 19:34:52 +0300

    swap default patient scan

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -14,19 +14,6 @@ export interface Patient {
 
 export const patients_example: Patient[] = [
   {
-    first_name: 'Peter',
-    last_name: 'Susser',
-    father_name: 'Abobovich',
-    sex: 'male',
-    birth_date: new Date(),
-    insurance: 'test',
-    country: 'Russia ',
-    city: 'Saint Petersburg',
-    street: 'Nevsky Avenue',
-    building: '114',
-    url: './ct.dcm'
-  },
-  {
     first_name: 'DICOM',
     last_name: 'CBCT.zip',
     father_name: '',
@@ -77,6 +64,19 @@ export const patients_example: Patient[] = [
     street: '',
     building: '',
     url: './DICOM/phantom.dcm.zip'
+  },
+  {
+    first_name: 'Peter',
+    last_name: 'Susser',
+    father_name: 'Abobovich',
+    sex: 'male',
+    birth_date: new Date(),
+    insurance: 'test',
+    country: 'Russia ',
+    city: 'Saint Petersburg',
+    street: 'Nevsky Avenue',
+    building: '114',
+    url: './ct.dcm'
   }
 ];
 export const makeId = (length: number) => {
